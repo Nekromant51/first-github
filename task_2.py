4b787234ce3d8cc241c4d75f5d4d408697109e75
Nekromant51 <boriskostin@bk.ru>
2023-03-05 09:20:17 +0600

    Задание № 3

---
+++ task_2.py
@@ -0,0 +1,12 @@
+def get_gcd(a):
+    gcd = 1
+    for i in range(1, a + 1):
+        if a % i == 0:
+            gcd = i
+        if gcd > 1:
+            break
+    return gcd
+
+
+number = int(input('Введите число: '))
+print('Наименьший делитель, отличный от единицы:', get_gcd(number))
